223e3987b9026c8448b8f6cb22db5a58cfd36b2b
Jonathan Pan <jpan127@users.noreply.github.com>
2017-10-24 22:30:14 -0700

    Added the front for the packet sending
    Added a note

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var Log = require('log'),
     log = new Log('debug')
  
 var port = process.env.PORT || 3000;
- 
+
 app.use(express.static(__dirname + "/public" ));
  
 app.get('/', function(req,res){
@@ -19,8 +19,18 @@ io.on('connection',function(socket){
 		socket.broadcast.emit('stream', image);
 	});
 	socket.on('event',function(data){
-		console.log('A client sent us this dumb message:', data.message);
+		console.log('A client sent us this SMART message:', data.message);
 	});
+    socket.on('exit', function(ignore) {
+        socket.disconnect(true);
+        console.log("Server disconnecting.");
+        process.exit();
+    });
+    socket.on('packet', function(args) {
+        console.log("Sending to", args.ip, ":", args.body);
+        // Needs support here to send to IP address
+        // Maybe the ESP32 has to be the server and the computer the client?
+    })
 });
 
 http.listen(port,function(){
